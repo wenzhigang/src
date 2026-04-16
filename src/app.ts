4d3a73ef97d6096a312d2a7d3f2117b4084b0737
zhigangwen <ares.wenzhigang@hotmail.com>
2026-04-16 23:32:05 +0800

    feat: init cloud development environment cloudbase-d7gl3kh5vf6b71edc

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import { PropsWithChildren } from 'react'
 import { useLaunch } from '@tarojs/taro'
-
+import Taro from '@tarojs/taro'
 import './app.scss'
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
-    console.log('App launched.')
+    // 初始化云开发环境
+    if (Taro.cloud) {
+      Taro.cloud.init({
+        env: 'cloudbase-d7gl3kh5vf6b71edc',
+        traceUser: true,
+      })
+      console.log('云开发环境初始化成功')
+    }
   })
 
-  // children 是将要会渲染的页面
   return children
 }
-  
-
 
 export default App
